0c44cf3915760c971b2a6472aa5f895b702f320f
晖 <zzh3258@qq.com>
2019-10-16 16:19:03 +0800

    add post method

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
+const querystring = require('querystring')
+
+const main = (req, res) => {
+    if (req.method == "GET") {
+        let { pathname, query } = url.parse(req.url, true);
+        console.log("pathname: ", pathname, "query: ", query);
+        fs.readFile("./static/" + req.url, (err, buffer) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.write(buffer);
+                res.end();
+            }
+        });
+    } else if (req.method == "POST") {
+        console.log("pathname: ",req.pathname);
+        let buffers = [];
+        req.on("data", buffer => {
+            buffers.push(buffer);
+        });
+        req.on("end", () => {
+            console.log(querystring.parse(buffers.toString()));
+        });
+    }
+};
+
 const apps = http.createServer((req, res) => {
-    // console.log("create success");
-    fs.readFile(`./${req.url}`, (err, buffer) => {
-        if (err) {
-            console.log(err);
-            res.writeHead(404);
-            res.end();
-        } else {
-            res.write(buffer);
-            res.end();
-        }
-
-
-        
-    });
+    main(req, res);
 });
 
-apps.listen(320, () => {
-    console.log("listen on port:320");
+apps.listen(300, () => {
+    console.log("listen on port:300");
 });
